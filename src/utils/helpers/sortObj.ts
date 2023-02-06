ec1cd238c5f326cf8b6d2339f10f31d971999bc1
yondav <yoni@yondav.us>
2023-02-05 20:20:40 -0500

    feat(g cz): utils/helpers
    adds helper function for sorting object alphabetically by key

---
+++ src/utils/helpers/sortObj.ts
@@ -0,0 +1,11 @@
+export default function sortObj(obj: Record<string, unknown>) {
+  return Object.keys(obj)
+    .sort()
+    .reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: obj[key],
+      }),
+      {}
+    );
+}
