da5c28f6721432b9018b7326f36649052d5d014f
yondav <yoni@yondav.us>
2023-02-05 20:25:32 -0500

    feat(tasks): exports all task functions

---
+++ src/tasks/index.ts
@@ -0,0 +1,5 @@
+export { default as copyDir } from './copyDir';
+export { default as exec } from './exec';
+export { default as makeDir } from './makeDir';
+export { default as removeTsComments } from './removeTsComment';
+export { default as writeFile } from './writeFile';
